68bff59499aed213cd64bb72b85ccf73e55a2cd5
James Coglan <jcoglan@gmail.com>
2013-06-12 01:34:51 +0100

    Clean up some process spawning code.

--- example/socket/server.js
+++ example/socket/server.js
@@ -6,7 +6,7 @@ var child     = require('child_process'),
 
 var root = __dirname + '/../..',
     bin  = root + '/build/bin/jstest',
-    spec = root + '/example/spec/set_spec.js'
+    argv = ['-f', 'json', root + '/example/spec/set_spec.js']
 
 var server = http.createServer()
 
@@ -17,8 +17,7 @@ server.on('upgrade', function(request, socket, body) {
       tests   = JSON.parse(params.test),
 
       options = tests.reduce(function(o, t) { return o.concat(['-t', t]) }, []),
-      argv    = ['-f', 'json'].concat(options).concat([spec]),
-      proc    = child.spawn(bin, argv)
+      proc    = child.spawn(bin, argv.concat(options))
 
   proc.stdout.pipe(split()).pipe(ws)
 })
