0c8ed0c42559e4726c8562e0edcbe81e6f5d3695
James Coglan <jcoglan@gmail.com>
2013-06-12 01:29:06 +0100

    Replace event-stream with split.

--- example/socket/server.js
+++ example/socket/server.js
@@ -1,7 +1,7 @@
 var child     = require('child_process'),
-    event     = require('event-stream'),
     http      = require('http'),
     url       = require('url'),
+    split     = require('split'),
     WebSocket = require('faye-websocket')
 
 var root = __dirname + '/../..',
@@ -18,10 +18,9 @@ server.on('upgrade', function(request, socket, body) {
 
       options = tests.reduce(function(o, t) { return o.concat(['-t', t]) }, []),
       argv    = ['-f', 'json'].concat(options).concat([spec]),
-      proc    = child.spawn(bin, argv),
-      split   = event.split('\n')
+      proc    = child.spawn(bin, argv)
 
-  proc.stdout.pipe(split).pipe(ws)
+  proc.stdout.pipe(split()).pipe(ws)
 })
 
 server.listen(8888)
