90eb2d8f8eacc33895f4e00d44f40882be4831c0
James Coglan <jcoglan@gmail.com>
2013-11-07 14:00:06 +0000

    Update Karma docs.

--- example/karma.js
+++ example/karma.js
@@ -1,9 +1,17 @@
 ROOT = 'http://localhost:4180'
 
 if (typeof window === 'undefined') {
-  basePath  = '..'
-  files     = ['example/karma.js', ROOT + '/build/jstest.js', ROOT + '/example/runner.js']
-  autoWatch = true
+  module.exports = function(config) {
+    config.set({
+      autoWatch: true,
+      basePath:  '..',
+      files: [
+        ROOT + '/example/karma.js',
+        ROOT + '/build/jstest.js',
+        ROOT + '/example/runner.js'
+      ]
+    })
+  }
 } else {
   __karma__.start = function() {}
 }
